5e3382a24e65c3306e452e2863289fdaceb5f43d
pachoyan <pachoyan@Paus-Mac-mini.local>
2024-04-14 14:32:39 -0600

    Fixed formula

--- Formula/hello-homebrew.rb
+++ Formula/hello-homebrew.rb
@@ -3,7 +3,7 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class HelloHomebrew < Formula
   desc "Hello World from Homebrew tap script"
-  homepage "ihttps://github.com/pachoyan/homebrew-scripts-tap"
+  homepage "https://github.com/pachoyan/homebrew-scripts-tap"
   url "https://gist.githubusercontent.com/pachoyan/aae2a96cb9a246f7e8af21cacc375637/raw/a7ef0d3db176804f0af3058a5bc8822cc57b7c4e/hello-homebrew.sh"
   version "0.1"
   sha256 "7c08c99e4ff776d0be807b0cc051198f745d5c5952482a974fba4c84b08faac4"
@@ -12,7 +12,7 @@ class HelloHomebrew < Formula
   depends_on "zsh" => :test 
 
   def install
-    bin.install "hello-homebrew.sh" => hello-homebew
+    bin.install "hello-homebrew.sh"
   end
 
 end
